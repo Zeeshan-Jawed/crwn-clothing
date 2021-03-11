42f2256c2790dc02d705e256315103938daf1974
Zeeshan <jawedzeeshan1@gmail.com>
2021-03-12 01:09:33 +0500

    Signin Component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component';
+import './sign-in.style.scss';
+
+class SignIn extends React.Component{
+constructor(){
+    super();
+    this.state={
+        email:'',
+        password:''
+    }
+}
+handleSubmit=event=>{
+    event.preventDefault();
+    this.setState({email:'',password:''});
+}
+handleChange=event=>{
+    const {name,value}=event.target
+    this.setState({[name]:value})
+}
+render(){
+    return(
+        <div className="sign-in">
+            <h2>I already have an account</h2>
+            <span>Signin with your email and password</span>
+            <form onSubmit={this.handleSubmit}>
+                <FormInput type="email" name="email" label="email" handleChange={this.handleChange} value={this.state.email} required />
+                
+                <FormInput type="password" name="password" label="password" handleChange={this.handleChange} value={this.state.password} required/>
+                
+                <CustomButton type="submit" > Sign In </CustomButton>
+            </form>
+            
+        </div>
+    )
+}
+
+}export default SignIn
--- src/config/Router.js
+++ src/config/Router.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import Homepage from '../pages/homepage/homepage.components'
 import Shop from '../components/shop/shop.component'
 import Header from '../components/header/header.component';
+import SignInAndSignUpPage from '../pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 class AppRouter extends React.Component{
     render(){
       return(
@@ -13,6 +14,7 @@ class AppRouter extends React.Component{
           <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={Shop} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
           </Switch>
       </Router>
       </div>
